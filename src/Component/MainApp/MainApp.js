38e327ea1089958e5285501e9fe8c032d22e7099
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2022-06-28 18:56:24 +0530

    user page

---
+++ src/Component/MainApp/MainApp.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+
+import './styles.css';
+
+const MainApp = () => {
+  const [users, setUsers] = useState([]);
+
+  const fetchApi = () => {
+    fetch('https://api.github.com/users')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setUsers(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+  useEffect(() => {
+    fetchApi();
+  }, []);
+  return (
+    <div className='box'>
+      {users.length > 0 ? (
+        users.map((user) => {
+          return (
+            <div className='card-container'>
+              <div className='card-img-container'>
+                <img
+                  className='card-img'
+                  src={user.avatar_url}
+                  alt=''
+                  srcset=''
+                />
+              </div>
+              <div className='card-user-data'>
+                <p>User name: {user.login}</p>
+                <p>{user.type} </p>
+              </div>
+            </div>
+          );
+        })
+      ) : (
+        <h2>No data available</h2>
+      )}
+    </div>
+  );
+};
+
+export default MainApp;
